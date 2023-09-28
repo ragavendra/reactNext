8fd93fda2e7753946a8d9cb8c9c59a0e56e33f83
Ragavendra <raga@example.com>
2023-09-28 14:08:30 -0700

    rvrd dtpu xunil

---
+++ public/index.jsx
@@ -0,0 +1,59 @@
+const app = document.getElementById('app');
+
+function Header({ title, some })
+// function Header(props)
+{
+  // console.log(title);
+  // return (<h1>Develop. Preview. Ship. 🚀</h1>);
+  // return <h1>{title}</h1>;
+  // return <h1>{props.title}</h1>;
+  // return <h1>{`Cool ${props.title}`}</h1>;
+  // return <h1>{createTitle(props.title)}</h1>;
+  // return <h1>{title ? some : 'Default Title'}</h1>;
+  return <h1>{title ? title : 'Default Title'}</h1>;
+}
+
+function createTitle(title)
+{
+  if(title)
+  {
+      return title;
+  }
+  else
+  {
+      return `Default title`;
+  }
+}
+
+function ManageLikes()
+{
+  const [likes, setLikes] = React.useState(6);
+
+  function handleClick()
+  {
+      console.log("incr like count");
+      setLikes(likes + 1);
+  }
+
+  return (
+          <button onClick={handleClick}>Likes {likes}</button>
+  )
+}
+
+function HomePage()
+{
+  const names = ["ant", 'elephant', 'horse'];
+  var i = 0;
+          // <Header />
+  return (
+      <div>
+          <Header title="React !" />
+          <Header title="New title - React !" some="ant data" />
+          <ul>
+              { names.map(name => ( <li key = {name + i++}>{name} <ManageLikes /></li> )) }
+          </ul>
+      </div>
+  );
+}
+
+ReactDOM.render(<HomePage />, app);
